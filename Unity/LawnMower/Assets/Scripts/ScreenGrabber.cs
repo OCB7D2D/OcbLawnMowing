e16ff7275cac5a60bca4b7e92a57f5c6acfa7a79
Marcel Greter <marcel.greter@ocbnet.ch>
2022-07-29 17:00:43 +0200

    Add unity project

---
+++ Unity/LawnMower/Assets/Scripts/ScreenGrabber.cs
@@ -0,0 +1,13 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+public class ScreenshotGrabber
+{
+    [MenuItem("Screenshot/Grab")]
+    public static void Grab()
+    {
+        ScreenCapture.CaptureScreenshot("Assets\\Screenshot.png", 1);
+    }
+}
+#endif
